0b4b46fcab5e44dd043543a81c084c5ac5988236
sojitramaulik <sojitramaulik518@gmail.com>
2026-05-12 16:08:13 +0530

    upgrade homepage

--- Frontend/src/components/ImageHome.tsx
+++ Frontend/src/components/ImageHome.tsx
@@ -2,20 +2,29 @@ import Box from "@mui/material/Box";
 import image from "../assets/library.jpg";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
+import { motion } from "framer-motion";
 
 export default function ImageHome() {
   return (
     <div>
       <Box
         sx={{
-          height: "90vh",
-          backgroundImage: `url(${image})`,
+          height: "100vh",
+          backgroundImage: `  linear-gradient(
+                to right,
+                rgba(0,0,0,0.85) 0%,
+                rgba(0,0,0,0.75) 35%,
+                rgba(0,0,0,0.65) 60%,
+                rgba(0,0,0,0.1) 100%
+              ), url(${image})`,
           backgroundSize: "cover",
           backgroundPosition: "center",
           backgroundRepeat: "no-repeat",
-
+          boxShadow: `
+              0 8px 32px rgba(0,0,0,0.35)
+            `,
           display: "flex",
-          justifyContent: "flex-start",
+          justifyContent: "center",
           alignItems: "start",
           paddingTop: "50px",
           paddingLeft: "50px",
@@ -23,18 +32,40 @@ export default function ImageHome() {
         }}
       >
         {/* Main Heading */}
-        <Typography
-          variant="h2"
-          sx={{
-            color: "#f8fafc",
-            fontWeight: "bold",
-            backgroundColor: "rgba(0,0,0,0.7)",
-            padding: "20px",
-            borderRadius: "20px",
+
+        <motion.div
+          initial={{ opacity: 0, x: -80 }}
+          animate={{ opacity: 1, x: 0 }}
+          transition={{ duration: 1 }}
+        >
+          <Typography
+            variant="h2"
+            sx={{
+              color: "#f8fafc",
+              fontWeight: "bold",
+              textShadow: `
+              0 0 8px rgba(255,255,255,0.5),
+              1px 1px 8px black
+            `,
+              // backgroundColor: "rgba(0,0,0,0.7)",
+              // padding: "20px",
+              // borderRadius: "20px",
+            }}
+          >
+            Read. Learn. Grow.! .......
+          </Typography>
+        </motion.div>
+
+        {/* Description */}
+
+        <motion.div
+          initial={{ opacity: 0, y: 40 }}
+          animate={{ opacity: 1, y: 0 }}
+          transition={{
+            duration: 1,
+            delay: 0.6,
           }}
         >
-          Read. Learn. Grow.! .......
-          {/* Description */}
           <Typography
             variant="h6"
             component="div"
@@ -42,11 +73,14 @@ export default function ImageHome() {
               color: "#f8fafc",
               marginTop: "20px",
               maxWidth: "600px",
-             // backgroundColor: "rgba(0,0,0,0.7)",
-              borderRadius: "10px",
-              padding: "20px",
+              // backgroundColor: "rgba(0,0,0,0.7)",
+              // borderRadius: "10px",
+              // padding: "20px",
               lineHeight: 1.8,
-              textShadow: "1px 1px 5px black",
+              textShadow: `
+              0 0 8px rgba(255,255,255,0.5),
+              1px 1px 8px black
+            `,
             }}
           >
             Discover a smarter way to manage books, students, and library
@@ -58,15 +92,23 @@ export default function ImageHome() {
               variant="contained"
               href="/auth/signup"
               sx={{
-                marginTop: "30px",
-                // marginLeft: "30px",
-                backgroundColor: "#0f4c5c",
+                borderRadius: "14px",
+                paddingX: "24px",
+                paddingY: "10px",
+                background: "linear-gradient(135deg,#14b8a6,#0f766e)",
+                boxShadow: "0 0 20px rgba(20,184,166,0.4)",
+                backgroundColor: "bg-[#084740]",
+                marginTop:"35px",
+                "&:hover": {
+                  transform: "translateY(-2px)",
+                  backgroundColor: "bg-[#03312c]",
+                },
               }}
             >
               Get Started
             </Button>
           </Typography>
-        </Typography>
+        </motion.div>
       </Box>
     </div>
   );
--- Frontend/src/components/Navbar.tsx
+++ Frontend/src/components/Navbar.tsx
@@ -3,12 +3,13 @@ import Box from "@mui/material/Box";
 import Toolbar from "@mui/material/Toolbar";
 import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
-import IconButton from "@mui/material/IconButton";
-import MenuIcon from "@mui/icons-material/Menu";
+// import IconButton from "@mui/material/IconButton";
+// import MenuIcon from "@mui/icons-material/Menu";
 import { useSelector } from "react-redux";
 import type { RootState } from "../redux/store";
 import Avatar from "@mui/material/Avatar";
 import { NavLink } from "react-router-dom";
+import { Library } from "lucide-react";
 
 export default function ButtonAppBar() {
 
@@ -24,9 +25,27 @@ export default function ButtonAppBar() {
 
   return (
     <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static" sx={{ backgroundColor: "#0f4c5c" }}>
+      <AppBar
+        position="static"
+        sx={{
+          backdropFilter: "blur(10px)",
+          background: "rgba(4, 47, 46, 0.7)",
+          borderBottom: "1px solid rgba(255,255,255,0.1)",
+          boxShadow: `
+                0 8px 32px rgba(0,0,0,0.35)
+              `,
+          backgroundColor: "rgba(4,47,46,0.75)",
+          border: "rgba(255, 255, 255, 0.08)",
+          position: "absolute",
+          top: 0,
+          left: 0,
+          width: "100%",
+          zIndex: 100,
+        }}
+        className="!flex !p-2"
+      >
         <Toolbar>
-          <IconButton
+          {/* <IconButton
             size="large"
             edge="start"
             color="inherit"
@@ -34,7 +53,10 @@ export default function ButtonAppBar() {
             sx={{ mr: 2 }}
           >
             <MenuIcon />
-          </IconButton>
+          </IconButton> */}
+
+          <Library size={32} color="#5eead4" />
+
           <div className="flex items-center justify-between w-full">
             <div className="flex justify-between">
               <div>
@@ -48,7 +70,9 @@ export default function ButtonAppBar() {
                   <NavLink
                     to="/add/book"
                     className={({ isActive }) =>
-                      isActive ? "text-white px-4 py-2 rounded" : "px-4 py-2"
+                      isActive
+                        ? "text-white bg-[#084740] px-4 py-2 rounded"
+                        : "px-4 py-2"
                     }
                   >
                     Book
@@ -56,7 +80,9 @@ export default function ButtonAppBar() {
                   <NavLink
                     to="/book/issue"
                     className={({ isActive }) =>
-                      isActive ? "text-white px-4 py-2 rounded" : "px-4 py-2"
+                      isActive
+                        ? "text-white bg-[#084740] px-4 py-2 rounded"
+                        : "px-4 py-2"
                     }
                   >
                     BookIssue
@@ -64,7 +90,9 @@ export default function ButtonAppBar() {
                   <NavLink
                     to="/dashboard"
                     className={({ isActive }) =>
-                      isActive ? "text-white px-4 py-2 rounded" : "px-4 py-2"
+                      isActive
+                        ? "text-white bg-[#084740] px-4 py-2 rounded"
+                        : "px-4 py-2"
                     }
                   >
                     BookFetch
@@ -74,20 +102,64 @@ export default function ButtonAppBar() {
             </div>
 
             {!user && (
-              <div>
-                <Button color="inherit" href="/auth/signup">
+              <div className="flex p-3 mr-10 gap-5">
+                <Button
+                  color="inherit"
+                  sx={{
+                    borderRadius: "14px",
+                    paddingX: "24px",
+                    background: "linear-gradient(135deg,#14b8a6,#0f766e)",
+                    boxShadow: "0 0 20px rgba(20,184,166,0.4)",
+                    backgroundColor: "bg-[#084740]",
+                    "&:hover": {
+                      transform: "translateY(-2px)",
+                      backgroundColor: "bg-[#03312c]",
+                    },
+                  }}
+                  href="/auth/signup"
+                >
                   SignUp
                 </Button>
-                <Button color="inherit" href="/auth/signin">
+                <Button
+                  color="inherit"
+                  sx={{
+                    borderRadius: "14px",
+                    paddingX: "24px",
+                    paddingY: "10px",
+                    background: "linear-gradient(135deg,#14b8a6,#0f766e)",
+                    boxShadow: "0 0 20px rgba(20,184,166,0.4)",
+                    backgroundColor: "bg-[#084740]",
+                    "&:hover": {
+                      transform: "translateY(-2px)",
+                      backgroundColor: "bg-[#03312c]",
+                    },
+                  }}
+                  href="/auth/signin"
+                >
                   Login
                 </Button>
               </div>
             )}
 
             {user && (
-              <div className="flex p-2">
+              <div className="flex gap-5 p-2">
                 <div className="p-2">
-                  <Button color="inherit" href="/auth/signup">
+                  <Button
+                    sx={{
+                      borderRadius: "14px",
+                      paddingX: "24px",
+                      paddingY: "10px",
+                      background: "linear-gradient(135deg,#14b8a6,#0f766e)",
+                      boxShadow: "0 0 20px rgba(20,184,166,0.4)",
+                      backgroundColor: "bg-[#084740]",
+                      "&:hover": {
+                        transform: "translateY(-2px)",
+                        backgroundColor: "bg-[#03312c]",
+                      },
+                    }}
+                    color="inherit"
+                    href="/auth/signup"
+                  >
                     Logout
                   </Button>
                 </div>
