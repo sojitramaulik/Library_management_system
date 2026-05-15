4213598e423f36ffd32763a4d5a655724e0ea869
sojitramaulik <sojitramaulik518@gmail.com>
2026-05-15 16:18:58 +0530

    add some cognito changes in app.ts

--- Frontend/src/auth.ts
+++ Frontend/src/auth.ts
@@ -1,7 +1,7 @@
 export const cognitoAuthConfig = {
-  authority: "https://cognito-idp.<region>.amazonaws.com/<USER_POOL_ID>",
+  authority: "https://us-east-17v9sdcnam.auth.us-east-1.amazoncognito.com",
   client_id: "g8ao4qhrj41napc6ov6coocko",
-  redirect_uri: "https://main.d271b2z7a8ilrw.amplifyapp.com",
+  redirect_uri: "https://us-east-17v9sdcnam.auth.us-east-1.amazoncognito.com",
   response_type: "code",
   scope: "openid email profile",
 };
